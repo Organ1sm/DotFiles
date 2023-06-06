f90fe6af5db916b8d780574df944895520aa2f5c
Organ1sm <ywwithpeople@gmail.com>
2023-06-06 23:59:48 +0800

    remove unused code

--- neovim/user/lsp.lua
+++ neovim/user/lsp.lua
@@ -7,27 +7,15 @@ local M = {
       allow_filetypes = {}, -- enable format on save for specified filetypes only
       ignore_filetypes = {}, -- disable format on save for specified filetypes,
     },
-    disabled = { -- disable formatting capabilities for the listed language servers
-      "lua_ls",
-      "neocmake",
-    },
+    disabled = {},
     timeout_ms = 2000, -- default format timeout
     -- filter = function(client) -- fully override the default formatting function
     --   return true
     -- end
   },
-  -- enable servers that you already have installed without mason
   servers = {},
-
-  setup_handlers = {
-    -- add custom handler
-    clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
-  },
-
-  config = {
-    clangd = require "user.lspconfig.clangd",
-    pyright = require "user.lspconfig.pyright",
-  },
+  setup_handlers = {},
+  config = {},
 }
 
 return M
