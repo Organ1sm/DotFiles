ebd936fbfd629c364209cc60a4413f167646d233
Organ1sm <ywwithpeople@gmail.com>
2023-06-09 23:03:52 +0800

    add dap config

--- neovim/user/plugins/dap.lua
+++ neovim/user/plugins/dap.lua
@@ -1,3 +1,35 @@
+local telescopeActions = require "telescope.actions"
+local telescopeActionsState = require "telescope.actions.state"
+local finders = require "telescope.finders"
+local pickers = require "telescope.pickers"
+local conf = require("telescope.config").values
+local fn = require "user.utils"
+
+local function run_func_on_file(table, opts)
+  opts = opts or {}
+
+  local drop_down = require("telescope.themes").get_dropdown {}
+  drop_down.attach_mappings = table.attach_mappings
+  opts = vim.tbl_deep_extend("force", drop_down, opts)
+  pickers
+      .new(opts, {
+        prompt_title = table.name,
+        finder = finders.new_table {
+          results = table.results(table.results_args),
+          entry_maker = function(entry)
+            return {
+              value = entry,
+              display = entry[1],
+              ordinal = entry[1],
+              path = entry[2],
+            }
+          end,
+        },
+        sorter = conf.file_sorter(opts),
+      })
+      :find()
+end
+
 return {
   "mfussenegger/nvim-dap",
   event = "VeryLazy",
@@ -33,10 +65,52 @@ return {
       },
     }
 
+    local function set_program()
+      local function run_scene(prompt_bufnr, map)
+        telescopeActions.select_default:replace(function()
+          telescopeActions.close(prompt_bufnr)
+          local selected = telescopeActionsState.get_selected_entry()
+          vim.g.dap_program = selected.path
+        end)
+        return true
+      end
+
+      run_func_on_file {
+        name = "Executable",
+        attach_mappings = run_scene,
+        results = fn.get_files_by_end,
+        results_args = fn.isWindows and ".exe" or "",
+      }
+      return true
+    end
+
+    require("astronvim.utils").set_mappings {
+      n = {
+        ["<leader>de"] = { set_program, desc = "Set program path" },
+      },
+    }
+
+    local lldb = {
+      name = "Launch",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        if not vim.g.dap_program or #vim.g.dap_program == 0 then
+          vim.g.dap_program = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end
+        return vim.g.dap_program
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+      args = {},
+    }
+
     -- C/C++
     dap.adapters.codelldb = {
+      name = "codelldb",
       type = "server",
       port = "${port}",
+      host = "127.0.0.1",
       executable = {
         -- CHANGE THIS to your path!
         command = CodeLLDBDir,
@@ -48,15 +122,7 @@ return {
     }
 
     dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "codelldb",
-        request = "launch",
-        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = true,
-        runInTerminal = true,
-      },
+      lldb,
     }
     dap.configurations.c = dap.configurations.cpp
 
--- neovim/user/utils.lua
+++ neovim/user/utils.lua
@@ -21,4 +21,16 @@ function M.getShell()
   end
 end
 
+function M.get_files_by_end(string) --> table
+  local find = vim.fs.find(function(x) return vim.endswith(x, string) end, { type = "file", limit = math.huge })
+  local files = {}
+  if #find > 1 then
+    for _, value in ipairs(find) do
+      local name = vim.fs.basename(value)
+      table.insert(files, { tostring(name), tostring(value) })
+    end
+  end
+  return files
+end
+
 return M
