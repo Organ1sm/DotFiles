4d18b745a72bf66a0cf1e83147194155edddb987
Organ1sm <ywwithpeople@gmail.com>
2023-07-31 22:53:07 +0800

    feat(nvim): add cmp-cmdline config

--- neovim/user/plugins/cmp.lua
+++ neovim/user/plugins/cmp.lua
@@ -1,8 +1,32 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+    },
     opts = function(_, opts)
       local lspkind_status_ok, lspkind = pcall(require, "lspkind")
+      local cmp = require "cmp"
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
 
       return require("astronvim.utils").extend_tbl(opts, {
         formatting = {
