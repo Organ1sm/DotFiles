beeb1fb718a99a9b809995f783dca8b0afc9abcb
Organ1sm <ywwithpeople@gmail.com>
2023-10-11 23:57:55 +0800

    [nvim]: update the new config for cmake-tools.nvim and clangd_extensions.nvim

--- neovim/user/plugins/clangd_extensions.lua
+++ neovim/user/plugins/clangd_extensions.lua
@@ -2,7 +2,7 @@ local utils = require "astronvim.utils"
 return {
   {
     "p00f/clangd_extensions.nvim",
-    init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "clangd") end,
+    -- init = function() utils.list_insert_unique(astronvim.lsp.skip_setup, "clangd") end,
     ft = { "c", "cpp" },
     lazy = true,
     dependencies = { "williamboman/mason-lspconfig.nvim" },
